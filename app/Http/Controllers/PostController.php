fdec7d3c449f4c9c478fdb0052ef778ae3532aa7
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-11-26 06:15:44 +0900

    feat: ポストの編集・削除機能を追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,16 +49,35 @@ class PostController extends Controller
 
     public function edit(Post $post)
     {
-        //
+        return view('post.edit', compact('post'));
     }
 
     public function update(Request $request, Post $post)
     {
-        //
+        $inputs = $request->validate([
+            'title' => 'required | max:255',
+            'body' => 'required | max:1000',
+            'image' => 'image | max:1024'
+        ]);
+
+        $post->title = $inputs['title'];
+        $post->body = $inputs['body'];
+
+        if(request('image')) {
+            $original = request()->file('image')->getClientOriginalName();
+            $name = date('Ymd_His').'_'.$original;
+            $file = request()->file('image')->move('storage/images', $name);
+            $post->image = $name;
+        }
+
+        $post->save();
+
+        return redirect()->route('post.show', $post)->with('message', '投稿を更新しました');
     }
 
     public function destroy(Post $post)
     {
-        //
+		$post->delete();
+		return redirect()->route('post.index')->with('message', '投稿を削除しました');
     }
 }
