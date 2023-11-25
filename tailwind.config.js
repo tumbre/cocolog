b7db8740914ce9caa1e6e51e5518270d46e1b301
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-11-26 01:36:33 +0900

    style: メニュー画面の変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                first: '#f3f4f6',
+                second: '#b2b2b2',
+                third: '#676767',
+                fourth: '#1d1d1f',
+                fifth: '#173260',
+                sixth: '#eb6038',
+                seventh: '#c1a26c'
+            }
         },
     },
 
