49457e4f9fa897446079f2fd19fbff3bbc96be19
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-11-26 01:34:24 +0900

    feat: ポストの新規投稿画面を作成

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,5 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('post', PostController::class);
 
 require __DIR__.'/auth.php';
