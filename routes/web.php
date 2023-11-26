2e7cc3792e52326e219154521544089ffd90fd63
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-11-26 22:03:43 +0900

    feat: ユーザー一覧機能を追加

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('profile/index', [ProfileController::class, 'index'])->name('profile.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'can:admin'])->group(function () {
+    Route::get('profile/index', [ProfileController::class, 'index'])->name('profile.index');
 });
 
 Route::resource('post', PostController::class);
