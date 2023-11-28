4835f1b3437bb504c91510d7e45046b3245efaad
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-11-28 22:25:25 +0900

    feat: 本番環境の画像の保存先をS3に変更

--- resources/views/components/preview.blade.php
+++ resources/views/components/preview.blade.php
@@ -2,7 +2,8 @@
 
 <div id="image-preview-container">
     <label for="image" class="font-semibold leading-none mt-6 mb-2">画像（1MBまで） </label>
-    <label id="add-image-button" class="mt-2 mb-4 bg-white rounded-full flex justify-center items-center border border-gray-400 hover:shadow-lg transition duration-300 ease-in-out cursor-pointer">
+    <label id="add-image-button"
+        class="mt-2 mb-4 bg-white rounded-full flex justify-center items-center border border-gray-400 hover:shadow-lg transition duration-300 ease-in-out cursor-pointer">
         <i class="fa-solid fa-camera"></i>
         <span class="ml-2 text-sm md:text-base">写真を選択する</span>
         <input id="image" type="file" name="image" class="hidden" multiple onchange="showPreview(event)">
@@ -11,9 +12,15 @@
     @isset($post)
         @if ($post->image)
             <div class="preview-image-wrapper relative">
-                <img id="existing-image" class="rounded-md mt-4 w-full" src="{{ asset('storage/images/'. $post->image) }}">
+                @if (app()->isLocal())
+                    <img src="{{ asset('storage/images/' . $post->image) }}" id="existing-image"
+                        class="rounded-md mt-4 w-full";>
+                @else
+                    <img id="existing-image" class="rounded-md mt-4 w-full" src="{{ $post->image }}">
+                @endif
                 <!-- 削除機能は後で実装 -->
-                <i id="remove-icon" class="fa-solid fa-circle-xmark fa-2xl remove-image absolute top-2 right-0 p-4 text-third cursor-pointer"></i>
+                <i id="remove-icon"
+                    class="fa-solid fa-circle-xmark fa-2xl remove-image absolute top-2 right-0 p-4 text-third cursor-pointer"></i>
             </div>
         @endif
     @endisset
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,34 +1,32 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="sm:text-2xl text-lg mb-2 text-fourth">{{ $post->title }}</h2>
-        <x-message :message="session('message')" />
     </x-slot>
 
-    <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
-            <div class="max-w-3xl">
-                <div class="max-w-3xl space-y-5 md:space-y-8">
-                    <div class="space-y-3">
-                        <div class="flex justify-end mt-4">
-                            <a href="{{ route('post.edit', $post) }}"><x-primary-button
-                                    class="bg-fifth float-right">編集</x-primary-button></a>
-                            <form method="post" action="{{ route('post.destroy', $post) }}">
-                                @csrf
-                                @method('delete')
-                                <x-primary-button class="bg-sixth float-right ml-4"
-                                    onclick="return confirm('本当に削除しますか？');">削除</x-primary-button>
-                            </form>
-                        </div>
-                        <div>
-                            @if ($post->image)
-                                <img src="{{ asset('storage/images/' . $post->image) }}" class="w-full mx-auto rounded-lg mb-2";>
-                            @endif
-                            <p class="mt-4 text-gray-600 py-4 whitespace-pre-line">{{ $post->body }}</p>
-                            <div class="text-sm font-semibold flex flex-row-reverse">
-                                <p>{{ $post->user->name }}・{{ $post->created_at->diffForHumans() }}</p>
-                            </div>
-                        </div>
-                    </div>
+    <div class="max-w-3xl mx-auto p-6 md:p-8 lg:p-12">
+        <div class="w-full space-y-5 md:space-y-8">
+            <div class="flex justify-end mt-4">
+                <a href="{{ route('post.edit', $post) }}"><x-primary-button
+                        class="bg-fifth float-right">編集</x-primary-button></a>
+                <form method="post" action="{{ route('post.destroy', $post) }}">
+                    @csrf
+                    @method('delete')
+                    <x-primary-button class="bg-sixth float-right ml-4"
+                        onclick="return confirm('本当に削除しますか？');">削除</x-primary-button>
+                </form>
+            </div>
+            <div>
+                @if ($post->image)
+                    @if (app()->isLocal())
+                        <img src="{{ asset('storage/images/' . $post->image) }}"
+                            class="w-full mx-auto rounded-lg mb-2";>
+                    @else
+                        <img src="{{ $post->image }}" class="w-full mx-auto rounded-lg mb-2">
+                    @endif
+                @endif
+                <p class="mt-4 text-gray-600 py-4 whitespace-pre-line">{{ $post->body }}</p>
+                <div class="text-sm font-semibold flex flex-row-reverse">
+                    <p>{{ $post->user->name }}・{{ $post->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
