b4335de07e6a6ea4d01282e6b9df15c2bec3c998
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-12-06 13:24:22 +0900

    style: ビューの調整

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
                     </div>
                     <p class="mb-4 font-semibold text-third md:mb-6 md:text-lg xl:text-xl">Let out everything that comes to your mind.</p>
                     <h1 class="mb-8 text-5xl md:text-6xl xl:text-7xl md:mb-12">こころ＋ログ。</h1>
-                    <p class="mb-8 leading-relaxed text-third md:mb-12 lg:w-4/5 xl:text-lg">cocologはこころのログを取るサービスです。<br>私たちは自分が思っているよりも自分のことを知りません。<br>気が付いたら同じところをぐるぐる回っていた、！なんてこともあるかもしれません。<br><br>cocologは、そんな状況を客観視する機会を提供します。<br>客観視することで、思考のループから抜け出すことができます。<br>ループから抜け出せれば、軽やかに行動できます。<br><br>今日の一日はどんな日でしたか？<br>まずは一行だけ、こころの言葉をアウトプットしてみましょう。</p>
+                    <p class="mb-8 leading-loose xl:leading-loose text-third md:mb-12 lg:w-4/5 xl:text-lg">cocologはこころのログを取るサービスです。<br>私たちは自分が思っているよりも自分のことを知りません。<br>気が付いたら同じところをぐるぐる回っていた、！なんてこともあるかもしれません。<br><br>cocologは、そんな状況を客観視する機会を提供します。<br>客観視することで、思考のループから抜け出すことができます。<br>ループから抜け出せれば、軽やかに行動できます。<br><br>今日の一日はどんな日でしたか？<br>まずは一行だけ、こころの言葉をアウトプットしてみましょう。</p>
                     @if(Auth::check())
                         <a href="{{ route('post.create') }}" class="flex flex-col gap-2.5 sm:flex-row sm:justify-center lg:justify-start">
                     @else
