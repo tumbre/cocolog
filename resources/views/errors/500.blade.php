a18c41939e64f75457169ffb0d9aee2eed168232
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-12-10 18:03:09 +0900

    chore: 画像のディレクトリを変更

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -51,7 +51,7 @@
             </div>
             <div class="relative flex-1 hidden w-0 lg:block min-h-screen">
                 <div class="absolute z-10 w-full h-full bg-white overflow-hidden">
-                    <img src="{{ asset('errors/osaka_art.jpg') }}" alt="" class="object-cover w-full h-full">
+                    <img src="{{ asset('images/osaka_art.jpg') }}" alt="" class="object-cover w-full h-full">
                 </div>
             </div>            
         </div>
