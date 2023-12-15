a208d45e113cc5cda52ea4b3ebe61d97adc53e82
ebiharatsukasa <ebiharatsukasa@gmail.com>
2023-12-16 02:09:58 +0900

    fix: ビューのセミコロンを削除

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,5 +5,5 @@
         <h2 class="sm:text-2xl text-lg mb-2 text-fourth font-semibold">{{ __('Write a diary') }}</h2>
     </x-slot>
 
-    @include('components.post-form');
+    @include('components.post-form')
 </x-app-layout>
